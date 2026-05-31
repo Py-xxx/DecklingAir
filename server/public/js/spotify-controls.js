23ec4d907d326dbfd2a814a2b142de3ff54ab23b
Py-xxx <berrnathan@gmail.com>
2026-05-31 17:33:10 +0200

    fix playback control alignments

--- server/public/js/spotify-controls.js
+++ server/public/js/spotify-controls.js
@@ -121,22 +121,22 @@ const SVG = {
     <path d="M19.07 4.93a10 10 0 0 1 0 14.14"/>
   </svg>`,
 
-  prev: () => `<svg width="22" height="22" viewBox="0 0 24 24" fill="currentColor"
+  prev: () => `<svg width="28" height="28" viewBox="0 0 24 24" fill="currentColor"
       xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
     <path d="M6 6h2v12H6zm3.5 6 8.5 6V6z"/>
   </svg>`,
 
-  next: () => `<svg width="22" height="22" viewBox="0 0 24 24" fill="currentColor"
+  next: () => `<svg width="28" height="28" viewBox="0 0 24 24" fill="currentColor"
       xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
     <path d="M6 18l8.5-6L6 6v12zm8.5-6v6h2V6h-2v6z"/>
   </svg>`,
 
-  play: () => `<svg width="26" height="26" viewBox="0 0 24 24" fill="currentColor"
+  play: () => `<svg width="30" height="30" viewBox="0 0 24 24" fill="currentColor"
       xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
     <path d="M8 5v14l11-7z"/>
   </svg>`,
 
-  pause: () => `<svg width="26" height="26" viewBox="0 0 24 24" fill="currentColor"
+  pause: () => `<svg width="30" height="30" viewBox="0 0 24 24" fill="currentColor"
       xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
     <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/>
   </svg>`,
